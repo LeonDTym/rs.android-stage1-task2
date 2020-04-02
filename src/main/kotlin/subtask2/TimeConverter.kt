522c33a6ed2040329dff87dbb5083684d0f1bc1e
Aliaksei Kisel <Aliaksei_Kisel@epam.com>
2020-04-02 17:10:37 +0300

    change signature timeFormatter

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -3,7 +3,7 @@ package subtask2
 class TimeConverter {
 
     // TODO: Complete the following function
-    fun toTextFormat(hour: String, minute: String): IntArray {
+    fun toTextFormat(hour: String, minute: String): String {
         throw NotImplementedError("Not implemented")
     }
 }
