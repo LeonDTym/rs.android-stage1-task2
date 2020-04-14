3bc1b473273e982eec7cd3bd8e33719f796896f1
LeonDTym <61317738+LeonDTym@users.noreply.github.com>
2020-04-14 14:45:34 +0300

    Update Fibonacci.kt

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,30 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        var array = IntArray(3)
+
+        var first = 0
+        var second = 1
+
+        while(true) {
+          if (first * second == n) {
+              array[0] = first
+              array[1] = second
+              array[2] = 1
+              break
+          } else if (first * second > n) {
+              array[0] = first
+              array[1] = second
+              array[2] = 0
+              break
+          }
+
+            val third = first + second
+            first = second
+            second = third
+        }
+
+        return array
     }
 }
