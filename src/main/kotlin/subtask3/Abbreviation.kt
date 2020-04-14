17f1089a417e6b3e2212d4e26602701256206b3d
LeonDTym <61317738+LeonDTym@users.noreply.github.com>
2020-04-14 14:42:58 +0300

    Update Abbreviation.kt

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,21 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val output = a.toUpperCase().toMutableList()
+        val charsToDelete = mutableListOf<Char>()
+
+        for (char in output) {
+            if (!b.contains(char)) {
+                charsToDelete.add(char)
+            }
+        }
+
+        for (char in charsToDelete) {
+            if (output.contains(char)) {
+                output.remove(char)
+            }
+        }
+        return if (output.joinToString("") == b) "YES" else "NO"
     }
 }
